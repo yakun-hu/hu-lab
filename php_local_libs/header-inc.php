fca4736158a16b81018ece706c207b216cb876f3
Yakun Hu <116873082+yakun-hu@users.noreply.github.com>
2023-05-21 22:38:47 -0400

    Add files via upload

---
+++ php_local_libs/header-inc.php
@@ -0,0 +1,14 @@
+<?php $research_full_element = '<span class="main-tab"><a style="color: white" href="research.main-test,1.php">Research</a>⬇️
+ <button class="sub-tab" style="position:absolute;top:254;left:268px;background-color:2daf26;height:40px"><a href="research-methods.php" style="color:#00ffff;">Research-Focus</a></button>
+ <button class="sub-tab" style="position:absolute;top:294;left:268px;background-color:2daf26;height:40px"><a href="research-methods.php" style="color:#00ffff;">Research-Achievement</a></button>
+ <button class="sub-tab" style="position:absolute;top:334;left:268px;background-color:2daf26;height:40px"><a href="research-methods.php" style="color:#00ffff;">Research-Support</a></button>
+ <button class="sub-tab" style="position:absolute;top:374;left:268px;background-color:2daf26;height:40px"><a href="research-methods.php" style="color:#00ffff;">Research-Methods</a></button></span>'; 
+$team_full_element = '<span class="main-tab"><a style="color: white" href="team.php">Team</a>⬇️<button class="sub-tab" style="position:absolute;top:254;left:60px;background-color:2daf26;height:40px">
+<a href="message-board.php?board=department&pg_数=1"style="color:#00ffff;">Announcements</a></button></span>'; 
+/* Necessary CSS-classes are written, and included, in lateral/CSS-external.php */ ?>
+
+<!-- test-url: http://personal-dash/php_local_libs/login.functions-inc.php -->
+<!-- issues-list-->	
+<!-- testing-log:-->
+<!-- references:
+	Necc.CSS-class: sub-tab, main-tab # -->
